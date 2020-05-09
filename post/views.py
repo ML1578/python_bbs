4423b92e481b230334b3277efd8e4aed7e733cfc
ML <3066947855@qq.com>
2020-05-09 13:48:14 +0800

    完成了帖子创建、阅读功能

---
+++ post/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+
+from post.models import Post
+# 发贴
+def create_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        post = Post.objects.create(title=title, content=content)
+        return redirect('/post/read/?post_id=%s' % post.id)
+    return render(request, 'create_post.html')
+
+
+# 修改帖子
+def edit_post(request):
+    return render(request, 'edit_post.html', {})
+
+
+# 阅读帖子
+def read_post(request):
+    post_id = int(request.GET.get('post_id'))
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'read_post.html', {'post': post})
+
+
+# 删除帖子
+def delete_post(request):
+    return render(request, 'delete_post.html', {})
